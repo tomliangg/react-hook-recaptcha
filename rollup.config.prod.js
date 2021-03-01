66bb24ad939d3f0682f0b602ae7c29fc009021f4
Tom Liang <tomliangg@gmail.com>
2021-02-28 22:27:18 -0800

    rollup prod config file

---
+++ rollup.config.prod.js
@@ -0,0 +1,36 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import pkg from './package.json';
+
+export default {
+  input: "src/useRecaptcha.js",
+  output: [
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true,
+    },
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    nodeResolve({
+      extensions: [".js", ".jsx"],
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'production' ),
+      preventAssignment: true,
+    }),
+    babel({
+      presets: ["@babel/preset-react"],
+    }),
+    commonjs(),
+  ]
+};
