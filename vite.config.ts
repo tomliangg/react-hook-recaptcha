394ee98df7048c1ce8887b0141dccf194d564445
Tom Liang <tomliangg@gmail.com>
2023-01-29 20:51:28 -0800

    vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/useRecaptcha.ts'),
+      name: 'react-hook-recaptcha',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'react',
+        },
+      },
+    },
+  },
 });
